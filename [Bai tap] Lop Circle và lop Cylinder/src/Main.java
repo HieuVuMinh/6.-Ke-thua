103d891776eb3f72c3bc1d700d84a6b79e11b111
unknown <vuhieu628383@gmail.com>
2021-04-09 17:04:52 +0700

    add file

---
+++ [Bai tap] Lop Circle và lop Cylinder/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Circle circle = new Circle(3, "blue");
+        Cylinder cylinder = new Cylinder(4, "red", 2);
+        System.out.println(circle);
+        System.out.println(cylinder);
+    }
+}
